73dc0ffaaec634e5225cf5dad9687028a39b90a5
imamilkman <mmspratt@icloud.com>
2015-07-20 19:41:39 -0500

    added remove slot imageViews

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Michael Spratt on 7/13/15.
+//  Copyright (c) 2015 Technolopy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
